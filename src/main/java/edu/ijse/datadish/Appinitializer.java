13b1e75c08b2f30e494d4862f08e70c2df1bd7ae
Vidura Priyadarshana <vidura200427@gmail.com>
2025-02-05 15:20:02 +0530

    DTOconverter updated

--- src/main/java/edu/ijse/datadish/Appinitializer.java
+++ src/main/java/edu/ijse/datadish/Appinitializer.java
@@ -16,7 +16,7 @@ public class Appinitializer extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent load = FXMLLoader.load((getClass().getResource("/view/Login.fxml")));
+        Parent load = FXMLLoader.load((getClass().getResource("/view/AdminDash.fxml")));
         stage.setScene(new Scene(load));
         Image icon = new Image(getClass().getResourceAsStream("/assests/logo.png"));
         stage.getIcons().add(icon);
--- src/main/java/edu/ijse/datadish/bo/DTOConverter.java
+++ src/main/java/edu/ijse/datadish/bo/DTOConverter.java
@@ -1,15 +1,59 @@
+//package edu.ijse.datadish.bo;
+//
+//import org.modelmapper.ModelMapper;
+//
+//public class DTOConverter {
+//    private static final ModelMapper modelMapper = new ModelMapper();
+//
+//    public static <D, E> E toEntity(D dto, Class<E> entityClass) {
+//        return modelMapper.map(dto, entityClass);
+//    }
+//
+//    public static <E, D> D toDTO(E entity, Class<D> dtoClass) {
+//        return modelMapper.map(entity, dtoClass);
+//    }
+//}
+
+
 package edu.ijse.datadish.bo;
 
-import org.modelmapper.ModelMapper;
+import java.lang.reflect.Field;
 
 public class DTOConverter {
-    private static final ModelMapper modelMapper = new ModelMapper();
-
     public static <D, E> E toEntity(D dto, Class<E> entityClass) {
-        return modelMapper.map(dto, entityClass);
+        try {
+            E entity = entityClass.getDeclaredConstructor().newInstance();
+            copyFields(dto, entity);
+            return entity;
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting DTO to Entity", e);
+        }
     }
 
     public static <E, D> D toDTO(E entity, Class<D> dtoClass) {
-        return modelMapper.map(entity, dtoClass);
+        try {
+            D dto = dtoClass.getDeclaredConstructor().newInstance();
+            copyFields(entity, dto);
+            return dto;
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting Entity to DTO", e);
+        }
+    }
+
+    private static void copyFields(Object source, Object target) throws IllegalAccessException {
+        Field[] sourceFields = source.getClass().getDeclaredFields();
+        Field[] targetFields = target.getClass().getDeclaredFields();
+
+        for (Field sourceField : sourceFields) {
+            sourceField.setAccessible(true);
+            for (Field targetField : targetFields) {
+                if (sourceField.getName().equals(targetField.getName()) &&
+                        sourceField.getType().equals(targetField.getType())) {
+                    targetField.setAccessible(true);
+                    targetField.set(target, sourceField.get(source));
+                    break;
+                }
+            }
+        }
     }
 }
